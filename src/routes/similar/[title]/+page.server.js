881979f6d4a60a3a55188f58064bcbd7bb9c363e
Veronika Gillies <veronikamwinters@gmail.com>
2023-09-08 22:20:18 +0000

    dedupe results

--- src/routes/similar/[title]/+page.server.js
+++ src/routes/similar/[title]/+page.server.js
@@ -1,6 +1,7 @@
 import { client, e } from "$lib/client"
 import distance from 'js-levenshtein'
 export async function load({ params }) {
+    let titles = {}
     let subnodes = await e
         .select(e.Subnode, () => ({
             title: true
@@ -8,7 +9,8 @@ export async function load({ params }) {
         .run(client)
     let matches = subnodes.map(({ title }) => {
         let dist = distance(params.title, title)
-        if (dist < 7) {
+        if (dist < 7 && !(titles[title])) {
+            titles[title] = true
             return { title, dist }
         }
     }).filter(x => x).sort((a, b) => a.dist - b.dist)
