6808b16ff8aab594ac52e43c9df2a279269e699e
Veronika Gillies <veronikamwinters@gmail.com>
2023-09-09 03:08:45 +0000

    human readable

--- src/routes/similar/[title]/+page.svelte
+++ src/routes/similar/[title]/+page.svelte
@@ -4,4 +4,8 @@
   let json = data.matches;
 </script>
 
-<JsonView {json} />
+<h1>Shorter distance is better</h1>
+{#each data.matches as match}
+  <div>[[<a href="/node/{match.title}">{match.title}</a>]] with distance of {match.dist}</div>
+{/each}
+<!-- <JsonView {json} /> -->
